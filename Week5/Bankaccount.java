238b6521bf9604adb71eb4e10635a93d7c39a2f3
Ziaul Hoque <c.rahulhoque@gmail.com>
2023-03-24 13:20:04 -0400

    co

---
+++ Week5/Bankaccount.java
@@ -0,0 +1,29 @@
+package Week5;
+
+    /*Appas bank
+Bank account Parameters
+ * Public Name =String
+ * Public Balamce=double
+ * Private SSn=int(Maybe String)
+ * APrivate ccount Number+int
+ * Private DOB
+ * 
+ * Bank accoiunt methods
+ * Deposit
+ * Withdraw
+ * Balance
+ * Getters andd setters
+ * Menu
+ * Compound Interest
+ * Simple Interest(Homework)
+ * 
+ * Bank acvount constructor
+ * 
+ */
+public class Bankaccount(){
+//Bank account parameters
+public string name;
+
+}
+
+}
---
+++ Week5/Bankaccounttester.java
@@ -0,0 +1,12 @@
+package Week5;
+
+public class Bankaccounttester {
+public static void main (String[]args){
+BankAccount myAccount =new BankAccount(name:"MOOMO",ssn:8979887);
+
+System.out.println(myAccout.name);
+System.out.println(myAccount.ssn);
+
+BankAccount yourAccount = new BankAccount()
+}
+}
