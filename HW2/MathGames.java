82af73cf9466394cebe3b8e376e9eeed56e68530
Ziaul Hoque <c.rahulhoque@gmail.com>
2023-03-24 18:29:21 -0400

    Hw2

--- HW2/MathGames.java
+++ HW2/MathGames.java
@@ -1,53 +1,60 @@
 package HW2;
 
-
 public class MathGames {
-    // 1. Create a method that will calculate the Pythagorean Theorem (https://en.wikipedia.org/wiki/Pythagorean_theorem)
-    // c = sqrt(a^2+b^2)
-    // NOTE: You can assume you will only need to calculate the Hypotenuse
-    // Hint 1: Method should take in 2 sides(a and b) of the triangle and return the missing side.
-    //      Remember the Triangle has a base, Perpendicular, and Hypotenuse
-    // Hint 2: Math.pow and Math.sqrt are your friend here
-    // Bonus points: Try to make it accept any 2 sides and return the 3rd. So it can take in
-    // Hypotenuse and Perpendicular and return the base.
-    // Example 1: pythagorean(3,4) would return 5
-
-
-
     
-    // 2. Create a method that will calcuate my grade in the class. You can use the grade range as follows
-        // A: 100 - 90
-        // B:  89 - 80
-        // C:  79 - 70
-        // D:  69 - 60
-        // F:  60 -  0 
-        // method should accept the grade as a double and return the letter grade of what I got
-        // NOTE: Im a really nice guy and always round up on my grading so a 69.3 will round up to a 70.0
-        // Hint 1: if statements required here you will probably need at the least 5 if / ifelse / else statments
-        // Hint 2: You will probably need to cast the double to an int
-        // Hint 3: You will probably need to use the Math.ceil() method
-        // Example: grade(69.3) would return a D
-
-
-
-
-
-
-    // 3. Create a method that will calculate the tip on a bill per person
-    //      Method parameters: total bill, amount of people splitting the bill, percentage to tip.
-    //      Note: I would like the total always rounded up to a whole number
-    //      Hint 1: percentage can be made easier to work with if you divide by 100 so 18% is 18/100 = 0.18
-    //      Hint 2: Using doubles will be helpful for when you need to calculate decimals
-    //      Hint 3: It is possible you might need to Cast a double to an int or vice versa
-    //      Hint 4: You will probably need to use the Math.ceil() method
-    //      Example: tip(100, 4, 18) would return 5.0
-
+    // 1. Method to calculate the Pythagorean Theorem
+    public static double pythagorean(double a, double b) {
+        double c = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
+        return c;
+    }
     
- 
-
-
-    // You can either create a tester class or put your code here
-     public static void main(String[] args){
+    // 2. Method to calculate the letter grade based on a given grade
+    public static String grade(double grade) {
+        int roundedGrade = (int) Math.ceil(grade);
+        String letterGrade;
         
+        if (roundedGrade >= 90) {
+            letterGrade = "A";
+        } else if (roundedGrade >= 80) {
+            letterGrade = "B";
+        } else if (roundedGrade >= 70) {
+            letterGrade = "C";
+        } else if (roundedGrade >= 60) {
+            letterGrade = "D";
+        } else {
+            letterGrade = "F";
+        }
+        
+        return letterGrade;
+    }
+    
+    // 3. Method to calculate the tip per person
+    public static double tip(double totalBill, int numPeople, double tipPercentage) {
+        double tipAmount = totalBill * (tipPercentage / 100.0);
+        double totalWithTip = totalBill + tipAmount;
+        double tipPerPerson = Math.ceil(totalWithTip / numPeople);
+        return tipPerPerson;
+    }
+
+    // Tester method to check the implementation
+    public static void main(String[] args){
+        // Pythagorean Theorem
+        double a = 3;
+        double b = 4;
+        double c = pythagorean(a, b);
+        System.out.println("The hypotenuse of the triangle with sides " + a + " and " + b + " is " + c);
+
+        // Grade calculation
+        double grade = 69.3;
+        String letterGrade = grade(grade);
+        System.out.println("The grade " + grade + " corresponds to the letter grade " + letterGrade);
+
+        // Tip calculation
+        double totalBill = 100.0;
+        int numPeople = 4;
+        double tipPercentage = 18.0;
+        double tipPerPerson = tip(totalBill, numPeople, tipPercentage);
+        System.out.println("If " + numPeople + " people split a bill of $" + totalBill + " with " + tipPercentage + "% tip, " 
+            + "each person would pay $" + tipPerPerson + " with the tip included.");
     }
 }
