54eb3ba14fc30fd4fbcd43ec2f2791e7e66efeab
Ziaul Hoque <c.rahulhoque@gmail.com>
2023-04-27 16:42:04 -0400

    HW4

--- HW4/Arrays.java
+++ HW4/Arrays.java
@@ -1,74 +1,97 @@
 package HW4;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Arrays {
 
-    // CONVERT THE SUMS METHOD FROM HW3 TO ADD EACH VALUE TO AN ARRAY AND THEN OUTPUT THE 
-    // ARRAY AND THE TOTAL.
-    // SEE HW3 FOR SUMS METHOD
-    // If you want to use an ArrayList for the inputs to be dynamic that is okay
-    // Otherwise if you are using a regular array, assume the size is 10 total. 
-    // EXAMPLE 
-    // I will add up the numbers you give me....
-    // Number: 12
-    // The total so far is 12.
-    // Number: 2
-    // The total so far is 14.
-    // Number: 3
-    // The total so far is 17.
-    // Number: 4
-    // The total so far is 21.
-    // Number: 1
-    // The total so far is 22.
-    // Number: 0
-    // Total: 22
-    // Array: {12,2,3,4,1,0}
-    public static void Arraysums(){
+     public static void Arraysums(){
+        Scanner input = new Scanner(System.in);
+        ArrayList<Integer> nums = new ArrayList<Integer>();
+        int total = 0;
+        int num = 0;
+        System.out.println("I will ask you to enter 10 numbers and then I will display the total and the array. ");
+        for(int i = 0; i < 10; i++){
+            System.out.println("Number: ");
+            num = input.nextInt();
+            nums.add(num);
+            total += num;
+            System.out.println("The total so far is " + total + ".");
+        }
+        
+        System.out.println("Total: " + total);
+        System.out.print("Array: {");
+        for(int i = 0; i < nums.size(); i++){
+            System.out.print(nums.get(i));
+            if(i < nums.size() - 1){
+                System.out.print(",");
+            }
+        }
+        System.out.print("}");
+         System.out.println("");
+         
+    }
+public static String bruteForce(String password){
+    String answer = "";
 
+    char[] alphabet = new char[]{'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+    '!', '@', '#', '$', '%', '^', '&', '*', '(', ')','<','>','/','?'};
+
+    // Convert the password to a character array
+    char[] passwordArray = password.toCharArray();
+
+    // Iterate through each index of the password array
+    for (int i = 0; i < passwordArray.length; i++) {
+        // Create a new string with the characters up to the current index
+        String currentAttempt = new String(passwordArray, 0, i + 1);
+
+        // Print the current attempt
+        System.out.println(currentAttempt);
+
+        // Check if the current attempt matches the password
+        if (currentAttempt.equals(password)) {
+            answer = currentAttempt;
+            break;
+        }
     }
 
-    // Create a method that will brute force a password EX.
-    // bruteForce("ZZZZ")
-    // The program should then guess each char of the string and compare it to see if it is equal
-    // to the index of the alphabet char array below
-    // Output Example:
-    // Z
-    // ZZ
-    // ZZZ
-    // ZZZZ
+    return answer; 
+}
 
-    // Hint 1: using a char array for the password is helpful here 
-    // Hint 2: using the method .toCharArray is a way to convert the password parameter to a char array as well
-    // From there creating for and while loops to compare the values is trivial
-
-    public static String bruteForce(String password){
-        String answer = "";
-        char[] alphabet = new char[]{'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
-        'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
-        '!', '@', '#', '$', '%', '^', '&', '*', '(', ')','<','>','/','?'};
-    
-        return answer; 
+public static int[] sorter(int[] nums){
+    int[] sortedArray = new int[nums.length];
+    for(int i = 0; i < nums.length; i++){
+        sortedArray[i] = nums[i];
     }
-
-    // Create a method that will sort an Array of integers from smallest to greatest.
-    // NOTE: You CANNOT use the built in Arrays.sort method
-    // Ex. 
-    // sorter([9,10,2,5,3,4,7,8]) -> [2,3,4,5,7,8,9,10]
-
-    // Hint 1: a temp Array is a helpful way to store variables for comparing 
-    // Hint 2: 2 nested For loops should be all thats needed for the swapping logic
-    public static int[] sorter(int[] nums){
-        int [] sortedArray = new int[nums.length];
-        return sortedArray;
+    int temp = 0;
+    for(int i = 0; i < sortedArray.length-1; i++){
+        for(int j = i+1; j < sortedArray.length; j++){
+            if(sortedArray[i] > sortedArray[j]){
+                temp = sortedArray[i];
+                sortedArray[i] = sortedArray[j];
+                sortedArray[j] = temp;
+            }
+        }
     }
-
-
-
-
-    public static void main(String[] args) {
-        // Tester main method for your methods
+    return sortedArray;
+}
+ public static void main(String[] args) {
+     
         System.out.println("Hello World!");
+        Arraysums();
+        bruteForce("Zzzz");
+        int[] myArray = {9,10,2,5,3,4,7,8};
+        int[]nums;
+         nums = sorter(myArray);
+         System.out.print("Array: {");
+        for(int i = 0; i < 8; i++){
+            System.out.print(nums[i]);
+                System.out.print(",");
+        }
+        System.out.print("}");
+       
 
     }
-
     
 }
